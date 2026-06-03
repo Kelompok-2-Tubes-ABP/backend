0443411c47bcfaf49217411fcefc98e5e9929955
Arsyad Maulana <arsyadmaulana@Arsyads-MacBook-Air-3.local>
2026-06-04 00:54:17 +0700

    feat: refactor GetMonthlyHandler to use query parameters for month and remove unnecessary bindings

--- essentials/handler/GetMonthlyHandler.go
+++ essentials/handler/GetMonthlyHandler.go
@@ -3,25 +3,21 @@ package handler
 import (
 	"financeapi/essentials/models"
 	"financeapi/essentials/services"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetMonthlyHandler(t *services.TransactionService) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var report models.Report
 		userID, exists := c.Get("user_id")
 		if !exists {
 			c.JSON(401, gin.H{"Error": "Not Authorized!!!"})
 			return
 		}
-		fmt.Printf("Looking for transactions with user_id = %s\n", userID)
 
-		if err := c.ShouldBindJSON(&report); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
+		month := c.DefaultQuery("month", "")
+
+		report := models.Report{Month: month}
 		monthly, errr := t.GetMonthly(userID.(string), report)
 
 		if errr != nil {
